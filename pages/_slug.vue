bd17836e52e529330f172d6eb6765a3db3bab378
Irfan Maulana <irfan.maulana@bizzy.co.id>
2019-02-22 08:37:06 +0700

    fix open-graph

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -102,12 +102,15 @@ export default {
         { hid: 'apple-mobile-web-app-title', name: 'apple-mobile-web-app-title', content: title },
 
         { hid: 'og:title', property: 'og:title', content: title },
-        { hid: 'og:description', name: 'og:description', content: description },
-        { hid: 'og:url', name: 'og:url', content: url },
+        { hid: 'og:description', property: 'og:description', content: description },
+        { hid: 'og:url', property: 'og:url', content: url },
+        { hid: 'og:type', property: 'og:type', content: 'article' },
+        { hid: 'article:published_time', property: 'article:published_time', content: new Date(this.meta.date).toISOString() },
+        { hid: 'article:section', property: 'article:section', content: 'Technology' },
 
         { hid: 'twitter:title', name: 'twitter:title', content: title },
         { hid: 'twitter:description', name: 'twitter:description', content: description },
-        { hid: 'og:url', name: 'og:url', content: url }
+        { hid: 'twitter:url', name: 'twitter:url', content: url }
       ]
     }
   },
