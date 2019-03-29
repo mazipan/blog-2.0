ed3138da23011fcbb4f6f3a592edc48e2f8b2d65
Irfan Maulana <irfan.maulana@bizzy.co.id>
2019-03-29 16:02:01 +0700

    chore: meta for mobile

--- contents/index.js
+++ contents/index.js
@@ -2,7 +2,7 @@
  * This file is for mapping url route that will be generated into html file
  * Should sort based on newest published date
  */
-module.exports = [
+export default [
   'panduan-unit-testing-di-vuejs',
   'membuat-blog-keren-dengan-gridsome',
   'pengalaman-menjadi-fasilitator-gdk-mws-2018',
