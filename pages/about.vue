425dbeb7b310348148e5deade828921d0029dba2
Irfan Maulana <irfan.maulana@bizzy.co.id>
2019-05-15 15:07:46 +0700

    fix(lint)

--- pages/about.vue
+++ pages/about.vue
@@ -4,7 +4,9 @@
       <h1>About</h1>
     </div>
     <div>
-      <img src="/images/profile-rounded.png" alt="@mazipan">
+      <img
+        src="/images/profile-rounded.png"
+        alt="@mazipan">
 
       <br>Hi, I am Irfan Maulana. Software Architect in Bizzy Indonesia, former Software Engineer in Blibli.com and SML Technologies.
       <br>An experience software engineer especially in frontend side. Having deep knowledge in HTML, CSS (with it's pre-processor) and JavaScript with various framework. Write good quality, well tested, and fast delivered code.
@@ -16,45 +18,45 @@
 
 <script>
 export default {
-  name: "AboutPage",
-  head() {
-    const title = `About | @mazipan`;
-    const description = `Page About`;
-    const url = `${this.productionUrl}/about/`;
+  name: 'AboutPage',
+  head () {
+    const title = `About | @mazipan`
+    const description = `Page About`
+    const url = `${this.productionUrl}/about/`
     return {
       title,
       meta: [
-        { hid: "description", name: "description", content: description },
+        { hid: 'description', name: 'description', content: description },
         {
-          hid: "apple-mobile-web-app-title",
-          name: "apple-mobile-web-app-title",
+          hid: 'apple-mobile-web-app-title',
+          name: 'apple-mobile-web-app-title',
           content: title
         },
 
-        { hid: "og:title", property: "og:title", content: title },
+        { hid: 'og:title', property: 'og:title', content: title },
         {
-          hid: "og:description",
-          property: "og:description",
+          hid: 'og:description',
+          property: 'og:description',
           content: description
         },
-        { hid: "og:url", property: "og:url", content: url },
+        { hid: 'og:url', property: 'og:url', content: url },
 
-        { hid: "twitter:title", name: "twitter:title", content: title },
+        { hid: 'twitter:title', name: 'twitter:title', content: title },
         {
-          hid: "twitter:description",
-          name: "twitter:description",
+          hid: 'twitter:description',
+          name: 'twitter:description',
           content: description
         },
-        { hid: "twitter:url", name: "twitter:url", content: url }
+        { hid: 'twitter:url', name: 'twitter:url', content: url }
       ]
-    };
+    }
   },
-  data() {
+  data () {
     return {
-      productionUrl: "https://www.mazipan.xyz"
-    };
+      productionUrl: 'https://www.mazipan.xyz'
+    }
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
