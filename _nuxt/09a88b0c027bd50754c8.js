d77d5db1397cf93a6a91969670673097ad10b5e0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-05 12:37:46 +0000

    Deploy mazipan/blog-2.0 to github.com/mazipan/blog-2.0.git:gh-pages

---
+++ _nuxt/09a88b0c027bd50754c8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{251:function(n,w,o){"use strict";o.r(w),w.default=[]}}]);
---
+++ _nuxt/a99567272c2560c1e687.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{249:function(n,w,o){"use strict";o.r(w),w.default=[]}}]);
