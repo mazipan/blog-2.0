758c67d9eab7e3acbb85f12b37a5d7db2de150c5
Irfan Maulana <maulana.irfan@tokopedia.com>
2019-08-27 23:59:25 +0700

    insert auto ads sample

--- plugins/vue-google-adsense.js
+++ plugins/vue-google-adsense.js
@@ -6,3 +6,17 @@ Vue.use(require('vue-script2'))
 Vue.use(Ads.Adsense)
 Vue.use(Ads.InArticleAdsense)
 Vue.use(Ads.InFeedAdsense)
+
+const head = document.head
+const s = document.createElement('script')
+s.type = 'text/javascript'
+s.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+s.onload = () => {
+  // eslint-disable-next-line
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: process.env.ADS_CLIENT,
+    enable_page_level_ads: true
+  })
+}
+
+head.appendChild(s)
