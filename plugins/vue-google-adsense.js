bd05d72ad43b2d107be2f15356f2072b1eb7db50
Irfan Maulana <maulana.irfan@tokopedia.com>
2019-08-28 10:11:52 +0700

    add autoAdsense

--- plugins/vue-google-adsense.js
+++ plugins/vue-google-adsense.js
@@ -3,20 +3,7 @@ import Ads from 'vue-google-adsense'
 
 Vue.use(require('vue-script2'))
 
+Vue.use(Ads.AutoAdsense, { adClient: process.env.ADS_CLIENT })
 Vue.use(Ads.Adsense)
 Vue.use(Ads.InArticleAdsense)
 Vue.use(Ads.InFeedAdsense)
-
-const head = document.head
-const s = document.createElement('script')
-s.type = 'text/javascript'
-s.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
-s.onload = () => {
-  // eslint-disable-next-line
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: process.env.ADS_CLIENT,
-    enable_page_level_ads: true
-  })
-}
-
-head.appendChild(s)
