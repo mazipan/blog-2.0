f0767ab78086aabb47d125a5405de99f48799edc
Irfan Maulana <maulana.irfan@tokopedia.com>
2019-08-28 10:32:05 +0700

    put data attr

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="container app">
+  <div
+    class="container app"
+    :data-enable-ads="ENABLE_ADS"
+    :data-ads-client="ADS_CLIENT"
+    :data-production-url="PRODUCTION_URL">
     <SimplifyHeader :amp="false" />
     <nuxt class="wrapper clearfix" />
 
@@ -18,6 +22,13 @@ import SimplifyHeader from '~/components/SimplifyHeader'
 import BaseFooter from '~/components/BaseFooter'
 import FormSubsription from '~/components/FormSubsription'
 import { trackJSPerf } from '~/utils/analitycs.js'
+
+import {
+  ENABLE_ADS,
+  ADS_CLIENT,
+  PRODUCTION_URL
+} from '~/constants'
+
 require('vue-ionicons/ionicons.css')
 
 export default {
@@ -26,6 +37,13 @@ export default {
     SimplifyHeader,
     BaseFooter,
     FormSubsription
+  },
+  data () {
+    return {
+      ENABLE_ADS,
+      ADS_CLIENT,
+      PRODUCTION_URL
+    }
   },
   mounted () {
     trackJSPerf()
