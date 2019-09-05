101628cc5b39cd2a6d7a78da25f1a7d1b890b953
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-05 18:12:42 +0000

    Deploy mazipan/blog-2.0 to github.com/mazipan/blog-2.0.git:gh-pages

--- sw.js
+++ sw.js
@@ -4,27 +4,43 @@ importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw
 // Configure
 // --------------------------------------------------
 
+
 // Set workbox config
 workbox.setConfig({
   "debug": false
 })
 
+
+
+
+
 // Start controlling any existing clients as soon as it activates
 workbox.core.clientsClaim()
+
+
 
 // Skip over the SW waiting lifecycle stage
 workbox.core.skipWaiting()
 
+
+
 workbox.precaching.cleanupOutdatedCaches()
+
+
 
 // Enable offline Google Analytics tracking
 workbox.googleAnalytics.initialize()
+
+
+
 
 // --------------------------------------------------
 // Precaches
 // --------------------------------------------------
 
 // Precache assets
+
+
 
 // -- Start of cachingExtensions --
 /* eslint-disable */
@@ -39,6 +55,7 @@ workbox.routing.registerRoute(
 )
 // -- End of cachingExtensions --
 
+
 // --------------------------------------------------
 // Runtime Caching
 // --------------------------------------------------
@@ -49,3 +66,8 @@ workbox.routing.registerRoute(new RegExp('^https://fonts.*(?:googleapis|gstatic)
 workbox.routing.registerRoute(new RegExp('^https://polyfill.io/(.*)'), new workbox.strategies.CacheFirst ({"cacheName":"Polyfill","cacheExpiration":{"maxEntries":2,"maxAgeSeconds":604800}}), 'GET')
 workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
 workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
+
+
+
+
+
